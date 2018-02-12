7f1fcda6d8261196136e353b6a03b913f7afdba3
HOMEPC\HomePC <insomnium62@gmail.com>
2018-02-12 21:41:12 +0300

    Init

---
+++ src/Main.java
@@ -0,0 +1,57 @@
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Message;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+
+public class Main extends TelegramLongPollingBot{
+    public static void main(String[] args) {
+        ApiContextInitializer.init(); // Инициализируем апи
+        TelegramBotsApi botapi = new TelegramBotsApi();
+        try {
+            botapi.registerBot(new Main());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+    @Override
+    public String getBotUsername() {
+        return "SberBotan_bot";
+        //возвращаем юзера
+    }
+
+    @Override
+    public void onUpdateReceived(Update e) {
+        Message msg = e.getMessage(); // Это нам понадобится
+        String txt = msg.getText();
+        if (txt.equals("/start")) {
+            sendMsg(msg, "Hello, world! This is simple bot!");
+        }
+        if (txt.equals("ВСП")) {
+            sendMsg(msg, "Внутреннее Структурное Подразделение");
+        }
+    }
+
+    @Override
+    public String getBotToken() {
+        return "476600922:AAHrTQiXjZVW5JMWgDHndO-evqMhVOpocO4";
+        //Токен бота
+    }
+
+    private void sendMsg(Message msg, String text) {
+        SendMessage s = new SendMessage();
+        s.setChatId(msg.getChatId()); // Боту может писать не один человек, и поэтому чтобы отправить сообщение, грубо говоря нужно узнать куда его отправлять
+        s.setText(text);
+        try { //Чтобы не крашнулась программа при вылете Exception
+            sendMessage(s);
+        } catch (TelegramApiException e){
+            e.printStackTrace();
+        }
+    }
+
+    enum Terms {
+        
+    }
+}
