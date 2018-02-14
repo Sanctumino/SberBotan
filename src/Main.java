421b35b00549bce230c0866c1faa00a116abc6af
HOMEPC\HomePC <insomnium62@gmail.com>
2018-02-14 23:29:29 +0300

    add package

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+package ru;
+
 import org.telegram.telegrambots.exceptions.TelegramApiException;
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.TelegramBotsApi;
