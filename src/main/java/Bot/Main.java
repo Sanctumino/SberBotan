d2267fcc4c0b280a4309f8ef7dce2b6226c9381f
HOMEPC\HomePC <insomnium62@gmail.com>
2018-02-21 00:03:03 +0300

    Set SQL Connection Add check null response

--- src/main/java/Bot/Main.java
+++ src/main/java/Bot/Main.java
@@ -11,7 +11,7 @@ import java.sql.*;
 import java.lang.String;
 
 public class Main extends TelegramLongPollingBot{
-    static final String DB_URL = "jdbc:postgresql://185.5.249.120/sber_botan";
+    static final String DB_URL = "jdbc:postgresql://185.5.249.120:5432/sber_botan";
     static final String USER = "sb";
     static final String PASS = "qwe123";
 
@@ -35,28 +35,49 @@ public class Main extends TelegramLongPollingBot{
         Message msg = e.getMessage(); // Это нам понадобится
         String txt = msg.getText();
         if (txt.equals("/start")) {
-            sendMsg(msg, "Hello, world! This is simple bot!");
-        }
-        /*else {
+            sendMsg(msg, "Привет! Введите аббревиатуру, без кавычек");
+        } else {
             try {
-                Connection connection = null;
-                connection = DriverManager.getConnection(DB_URL,USER,PASS);
+                Class.forName("org.postgresql.Driver");
+            } catch (ClassNotFoundException ep) {
+                System.out.println("Where is your PostgreSQL JDBC Driver? "
+                        + "Include in your library path!");
+                ep.printStackTrace();
+                return;
+            }
+            System.out.println("PostgreSQL JDBC Driver Registered!");
+            Connection connection = null;
+            try {
+                connection = DriverManager.getConnection(DB_URL, USER, PASS);
                 Statement request = connection.createStatement();
                 ResultSet response = request.executeQuery("select definition from dictionary where abbreviation = '" + txt + "'");
-                sendMsg(msg,response.toString());
+                int count = 0;
+                while (response.next()) {
+                        String str = response.getString(1);
+                        sendMsg(msg, str);
+                        count++;
+                        }
+                if (count==0){
+                    sendMsg(msg, "Аббреваиатура не найдена.");
+                }
                 response.close();
                 request.close();
             }
-            catch (SQLException ex) {
-                System.out.println("Connection failed");
+             catch (SQLException ex) {
+                System.out.println("Connection Failed! Check output console");
+                ex.printStackTrace();
                 return;
             }
-        }*/
-        if (txt.equals("ВСП")) {
+            if (connection != null) {
+                System.out.println("You made it, take control your database now!");
+            } else {
+                System.out.println("Failed to make connection!");
+            }
+       /* if (txt.equals("ВСП")) {
             sendMsg(msg, "Внутреннее Структурное Подразделение");
+        }*/
         }
     }
-
     @Override
     public String getBotToken() {
         return "476600922:AAHrTQiXjZVW5JMWgDHndO-evqMhVOpocO4";
